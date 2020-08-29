104f572eaf267bcad2bb23a32327defcdafb6030
awp <awpxwork@gmail.com>
2020-08-29 20:12:00 +0700

    add temporary home pages & product comp

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
+import { Home } from './pages/Home'
+
 
 const App = () => {
   return (
@@ -9,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
       <Container>
-        main
+        <Home />
       </Container>
       </main>
       <Footer />
