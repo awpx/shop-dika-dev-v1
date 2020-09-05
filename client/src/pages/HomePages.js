8c9766287d938cb1f3b93edc0d161fb2b4a8da93
awp <awpxwork@gmail.com>
2020-09-05 14:44:00 +0700

    add product list reducer

--- client/src/pages/HomePages.js
+++ client/src/pages/HomePages.js
@@ -1,29 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import { Product } from '../components/Product'
+import { listProducts } from '../actions/productActions'
 
 export const HomePages = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/v1/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+
+  }, [dispatch])
 
   return (
     <>
       <h2>Latest Products</h2>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading
+        ? ( <h3>Loading...</h3> )
+        : error
+        ? ( <h4>{error}</h4> )
+        : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        )  
+      }
+      
     </>
   )
 }
