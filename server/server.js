ce1ad76abb8d7a921ead8e7c3f55f90f0af1264d
awp <awpxwork@gmail.com>
2020-09-08 21:15:00 +0700

    added auth jwt, register(postman)

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
 
 connectDB()
 
@@ -16,7 +19,9 @@ app.get('/', (req, res) => {
   res.send('api services running')
 })
 
+//routes
 app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/users', userRoutes)
 
 //middleware error handler
 app.use(notFound)
