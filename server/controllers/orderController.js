020024d875161456759806feec55a62f9c0ad1bb
awp <awpxwork@gmail.com>
2020-09-15 11:01:00 +0700

    order details func

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
-//@desc       Fetch create new order
+//@desc       create new order
 //@route      POST /api/v1/orders
 //@access     private
 export const addOrderItems = asyncHandler( async (req, res) => {
@@ -34,5 +34,22 @@ export const addOrderItems = asyncHandler( async (req, res) => {
     const createdOrder = await order.save()
 
     res.status(201).json(createdOrder)
+  }
+})
+
+
+//@desc       get order by id
+//@route      POST /api/v1/orders/:id
+//@access     private
+export const getOrderById = asyncHandler( async (req, res) => {
+  const order = await Order
+    .findById(req.params.id)
+    .populate('user', 'name email')
+
+  if(order){
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
   }
 })
