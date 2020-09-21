ac098a8939869ced44f15156f5d77fb8a57bfc5d
awp <awpxwork@gmail.com>
2020-09-21 17:32:00 +0700

    create & update product backend

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -42,3 +42,59 @@ export const deleteProduct = asyncHandler( async (req, res) => {
     throw new Error('Product not found')
   }
 })
+
+//@desc       Admin: create product
+//@route      POST /api/v1/products/
+//@access     private/admin
+export const createProduct = asyncHandler( async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+//@desc       Admin: update product
+//@route      PUT /api/v1/products/:id
+//@access     private/admin
+export const updateProduct = asyncHandler( async (req, res) => {
+  const { 
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock 
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+
+  } else{
+    res.status(404)
+    throw new Error('Product not found')
+  }
+
+  
+})
--- server/models/productModel.js
+++ server/models/productModel.js
@@ -28,6 +28,10 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  brand: {
+    type: String,
+    required: true,
+  },
   category: {
     type: String,
     required: true,
