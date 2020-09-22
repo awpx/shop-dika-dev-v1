0d74a21df3728c6d0ae8516bd71d211632f72d1c
awp <awpxwork@gmail.com>
2020-09-22 21:32:00 +0700

    multer config

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -7,6 +8,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -24,11 +26,16 @@ app.get('/', (req, res) => {
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/orders', orderRoutes)
+app.use('/api/v1/upload', uploadRoutes)
 
 //paypall
 app.get('/api/v1/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+//static
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 //middleware error handler
 app.use(notFound)
