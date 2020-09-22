0d47ef0231075c620228f26d2e8478e0ed369778
awp <awpxwork@gmail.com>
2020-09-22 23:41:00 +0700

    frontend upload img

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -24,7 +24,7 @@ export const Product = ({ product }) => {
         />
       </Card.Text>
 
-      <Card.Text as='h3'> {product.price} </Card.Text>
+      <Card.Text as='h3'>IDR {product.price} </Card.Text>
 
       </Card.Body>
 
